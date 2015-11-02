03131afbd6d3d834880bcf37befd623fd73c9bcc
FRACHET Marvin <FRACHET@dev.groupe.adg>
2015-11-02 14:49:21 +0100

    change doc

--- src/app/core/logger/LoggerFactory.js
+++ src/app/core/logger/LoggerFactory.js
@@ -7,6 +7,7 @@ export default class LoggerFactory{
   /**
   * Get a logger from its name or return null
   * @param {String} name Name of the logger
+  * @param {Object} params Parameters of the logger
   * @return {Object} The logger object or null if doesn't exist
   */
   static get(name, params){
