eec15cdebb7703ff3f1ef18906813db7ce245486
Marvin Frachet <marvin.frachet@zenika.com>
2017-04-13 08:46:50 +0200

    Fix bug related on the dist path

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,28 @@
 var gulp = require('gulp'),
-    babel = require('gulp-babel'),
-    uglify = require('gulp-uglify'),
-    watch = require('gulp-watch'),
-    plumber = require('gulp-plumber'),
-    del = require('del');
+	uglify = require('gulp-uglify'),
+	babel = require('gulp-babel'),
+	plumber = require('gulp-plumber'),
+	del = require('del');
 
 
 require('babel-core/register');
 
 
-gulp.task('default', ['conf'], function() {
-    return gulp.src('src/**/*.js')
-        .pipe(plumber())
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+gulp.task('default', ['conf'], function () {
+	return gulp.src('src/app/**/*.js')
+		.pipe(plumber())
+		.pipe(babel())
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('conf', ['clean'], function() {
-    return gulp.src('./src/conf/*')
-        .pipe(gulp.dest('dist/conf'));
+gulp.task('conf', ['clean'], function () {
+	return gulp.src('./src/conf/*')
+		.pipe(gulp.dest('dist/conf'));
 });
 
-gulp.task('clean', function() {
-    return del([
-        'dist/**/*'
-    ]);
+gulp.task('clean', function () {
+	return del([
+		'dist/**/*'
+	]);
 });
--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,5 @@
-import ConfReader from './app/core/conf/confReader'
-import LoggerFactory from './app/core/logger/loggerFactory'
+import {ConfReader} from './core/conf/confReader'
+import {LoggerFactory} from './core/logger/loggerFactory'
 /**
 * Application class, Entry point of the application
 */
@@ -9,12 +9,12 @@ class Application {
   * The main method, the entry point of the application
   * @param {Object} argv Arguments from process.argv
   */
-  static main (argv, path) {
-    const configuration = new ConfReader().read(path + '/conf/' + argv[2] + '.yml')
+  static main (argv) {
+    const configuration = new ConfReader().read(__dirname + '/conf/' + argv[2] + '.yml')
     const logger = LoggerFactory.get('bunyan', {name: configuration.application.name})
     logger.info('Super app is started !')
   }
 }
 
 // We run the main method of the Application class
-Application.main(process.argv, './dist')
+Application.main(process.argv)
