3a824944242707bb36277c98fa70f0e2188f1118
Marvin Frachet <marvin.frachet@zenika.com>
2017-04-14 17:00:06 +0200

    Fixing code style standard

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
-const gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
-	babel = require('gulp-babel'),
-	sourcemaps = require('gulp-sourcemaps'),
-	plumber = require('gulp-plumber'),
-	del = require('del');
+const gulp = require('gulp')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const plumber = require('gulp-plumber')
+const del = require('del')
 
 gulp.task('default', ['conf'], () => gulp.src('src/app/**/*.js')
-	.pipe(plumber())
-	.pipe(babel())
-	.pipe(uglify())
-	.pipe(gulp.dest('dist')));
+  .pipe(plumber())
+  .pipe(babel())
+  .pipe(uglify())
+  .pipe(gulp.dest('dist')))
 
-gulp.task('conf', ['package'], () => gulp.src('./src/conf/*').pipe(gulp.dest('dist/conf')));
+gulp.task('conf', ['package'], () => gulp.src('./src/conf/*').pipe(gulp.dest('dist/conf')))
 
-gulp.task('package', ['clean'], () => gulp.src(['./package.json']).pipe(gulp.dest('dist/')));
+gulp.task('package', ['clean'], () => gulp.src(['./package.json']).pipe(gulp.dest('dist/')))
 
-gulp.task('clean', () => del(['dist/**/*']));
+gulp.task('clean', () => del(['dist/**/*']))
